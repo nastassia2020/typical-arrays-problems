af0871907e47d77a5211c6bc27212991ffaa6493
Shylko Nastassia <wcganastacia@yandex.ru>
2020-10-03 10:15:27 +0300

    feat: add min, max, avg func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array.length == 0 || array == 0){
+    return 0;
+  }else{
+    let min = Math.min.apply(null, array)
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array.length == 0 || array == 0){
+    return 0;
+  }else{
+    let max = Math.max.apply(null, array)
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array.length == 0 || array == 0){
+    return 0;
+  }else{
+  const sum = array.reduce((x, y) => x + y);
+  const avg = sum / array.length;
+    return avg;
+  }
 }
